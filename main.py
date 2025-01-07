d46c0fd9ecc2c218327bc11fb6df3c4ae768c14d
Vision295 <78678991+Vision295@users.noreply.github.com>
2025-01-07 18:23:25 +0100

    testing

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-import GNS3FY
+import gns3fy
+
+server = gns3fy.Gns3Connector("http://localhost:3080")
 
 
+lab = gns3fy.Project(name="testprojetGNS", connector=server)
+lab.get()
 
-print("Hello World")
+print(f"Name: {lab.name} -- Status: {lab.status} -- Is auto_closed?: {lab.auto_close}")
+lab.open()
+
+print(lab.stats)
+
+
+for node in lab.nodes:
+    print(f"Node: {node.name} -- Node Type: {node.node_type} -- Status: {node.status}")
+    print(node.console)
+    
