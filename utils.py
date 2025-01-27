13a95540b4f22333d55c369c7c4b4e50ea315126
polorados <paul.treutenaere@gmail.com>
2025-01-27 17:13:10 +0100

    petit changement dans utils

--- utils.py
+++ utils.py
@@ -2,7 +2,8 @@ import ipaddress
 
 
 get_network = lambda addr: str(ipaddress.IPv6Network(addr, strict=False).network_address) + '/' + str(ipaddress.IPv6Network(addr, strict=False).prefixlen)
-without_net_suffix = lambda addr : addr[:-4]
+
+without_net_suffix = lambda addr: addr.split('/')[0]
 
 def get_interface_name(interface_shortcut:str) -> str:
       match(interface_shortcut):
