1779ec59be166cd63b0b1d4e03f95928f61fdb0d
Vision295 <theophilewachtel@gmail.com>
2025-01-27 16:40:00 +0100

    trois fois rien

--- utils.py
+++ utils.py
@@ -1,7 +1,17 @@
+import ipaddress
 
 
+#get_network = lambda addr: addr[:-5] + '0' + addr[-4:]
+def get_network(addr:str) -> str:
+      mid = addr.index('/')
+      network_size = int(addr[mid+1:])
+      
+      ipaddress.ip_network()
+      
+      return network_size
 
-get_network = lambda addr: addr[:-5] + '0' + addr[-4:]
+print(get_network("123:123:123::123/127"))
+
 without_net_suffix = lambda addr : addr[:-4]
 
 def get_interface_name(interface_shortcut:str) -> str:
