429ff4eb79507da9075cd4ca02b75e62ae2fb73b
Vision295 <theophilewachtel@gmail.com>
2025-01-30 08:22:32 +0100

    c'est bon intent ça marche !

--- intentGen.py
+++ intentGen.py
@@ -38,13 +38,15 @@ class IntentGen():
       
       def get_networks(self):
             self.networks = {}
+            nb_subnets = len(self.beta)
+            self.subnets = list(islice(self.ipRange.subnets(new_prefix=PREFIX), 0, nb_subnets))
       
             nb_subnet_taken = 0
             for index, value in enumerate(self.alpha):
                   for i, v in enumerate(self.beta[self.alpha[index-1]:value+1]):
                         key = (index, v)
                              
-                        if not key in networks.keys() and not key in networks.keys() and key[0] != key[1]:
+                        if not key in self.networks.keys() and not key in self.networks.keys() and key[0] != key[1]:
                               self.networks[key] = self.subnets[nb_subnet_taken].compressed
                               nb_subnet_taken += 1
 
@@ -73,11 +75,11 @@ class IntentGen():
 
             interfaces = ["f0/0", "g1/0", "g2/0", "g3/0"]
 
-            for key, value in connections.items():
+            for key, value in self.connections.items():
                   self.what_to_add[str(key)] = {interfaces[i]: v for i, v in enumerate(value)}
                   
-            new_intent = copy.deepcopy(data)
-            for key, value in self.data["AS"].items():
+            self.new_intent = copy.deepcopy(self.data)
+            for key, value in self.data['AS'].items():
                   routers_to_loop = value['routers']
                   self.new_intent['AS'][key]['routers'] = {}
                   for i in routers_to_loop:
@@ -86,68 +88,4 @@ class IntentGen():
       def write_on_intent(self, name:str): 
             with open(name+".json", "w") as json_file:
                   json.dump(self.new_intent, json_file, indent=6)
-                  
-            
-networks = {}
-
-ipRange = ipaddress.ip_network(input("Enter the IP network for the range like this XXXX:...:0/Y where Y < 64 : "))
-
-with open('intent2.json', 'r') as file : data = json.load(file)
-
-alpha, beta = data["alpha"], data["beta"]
-nb_subnets = len(beta) #if no duplicates
-subnets = list(islice(ipRange.subnets(new_prefix=PREFIX), 0, nb_subnets))
-
-nb_subnet_taken = 0
-for index, value in enumerate(alpha):
-      for i, v in enumerate(beta[alpha[index-1]:value+1]):
-            key = (index, v)
-                 
-            if not key in networks.keys() and not key in networks.keys() and key[0] != key[1]:
-                  networks[key] = subnets[nb_subnet_taken].compressed
-                  nb_subnet_taken += 1
-            
-print("networks : ", networks)
-            
-connections = {}
-for index, value in networks.items():
-      if index[0] in connections.keys():
-            connections[index[0]].append((ipaddress.ip_interface(value) + 1).compressed)    
-      else:
-            connections[index[0]] = [(ipaddress.ip_interface(value) + 1).compressed]
-      
-      if index[1] in connections.keys():
-            connections[index[1]].append((ipaddress.ip_interface(value) + 2).compressed)
-      else:
-            connections[index[1]] = [(ipaddress.ip_interface(value) + 2).compressed]
-
-
-for values in connections.values():
-      if len(values) > 4:
-            print("Router cannot have more than 4 connections !")
-            
-print("connections : ", connections)
-
-
-
-intent = {}            
-
-interfaces = ["f0/0", "g1/0", "g2/0", "g3/0"]
-
-for key, value in connections.items():
-      intent[str(key)] = {interfaces[i]: v for i, v in enumerate(value)}
-      
-new_intent = copy.deepcopy(data)
-for key, value in data["AS"].items():
-      routers_to_loop = value['routers']
-      new_intent['AS'][key]['routers'] = {}
-      for i in routers_to_loop:
-            new_intent['AS'][key]['routers'][i] = intent[str(i)]
-
-print("new_intent : ", new_intent)
-
-print("intent : ", intent)
-with open("intent3.json", "w") as json_file:
-      json.dump(new_intent, json_file, indent=6)
-
-
+                              
