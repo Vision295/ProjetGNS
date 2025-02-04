91cb750f002b6536afe6526534e9bdfd8755327e
Vision295 <theophilewachtel@gmail.com>
2025-02-04 16:33:16 +0100

    avec @ de loopback c'est mieux

--- utils.py
+++ utils.py
@@ -22,29 +22,4 @@ def get_interface_name(interface_shortcut:str) -> str:
             case "g3/0" : return "Gigabitethernet2/0"
             case "f0/0" : return "Fastethernet0/0"
             case _ : return ""
-            
-def get_border_router_ips(intent: dict) -> list[tuple[str, str]]:
-    border_routers = []
-    as_data = intent["AS"]
-    
-    for asn, as_info in as_data.items():
-        for router_id, interfaces in as_info["routers"].items():
-            for iface, ip in interfaces.items():
-                current_network = ipaddress.ip_interface(ip).network
-                
-                for other_asn, other_as_info in as_data.items():
-                    if other_asn != asn:
-                        for other_router, other_interfaces in other_as_info["routers"].items():
-                            for other_ip in other_interfaces.values():
-                                other_network = ipaddress.ip_interface(other_ip).network
-                                if current_network.network_address == other_network.network_address:
-                                    border_routers.append((ip.split('/')[0], other_asn))
-    
-    return border_routers
 
-
-
-
-import json
-with open("intent3.json") as file:
-    print(get_border_router_ips(json.load(file)))
