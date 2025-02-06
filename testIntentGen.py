1bfd97f2a997624e57034b32315f916285e8561f
Vision295 <theophilewachtel@gmail.com>
2025-02-06 09:50:14 +0100

    ménage

--- testIntentGen.py
+++
@@ -1,8 +0,0 @@
-from intentGen import IntentGen
-import json
-
-
-with open('intent2.json', 'r') as file : data = json.load(file)
-intentGen = IntentGen(data)
-intentGen.gen()
-intentGen.write_on_intent("intent4")
